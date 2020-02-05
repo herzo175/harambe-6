22790f70874f02f8365bd815feaba547543b5d7f
Jeremy Herzog <jherzog@corebiome.com>
2020-02-05 16:30:00 -0600

    try passing token?

--- service.tf
+++ service.tf
@@ -90,8 +90,7 @@ resource "kubernetes_cluster_role_binding" "tiller" {
 provider "helm" {
   kubernetes {
     host     = data.digitalocean_kubernetes_cluster.harambe.endpoint
-    # username = "ClusterMaster"
-    # password = "MindTheGap"
+    token = data.digitalocean_kubernetes_cluster.harambe.kube_config[0].token
 
     client_certificate     = base64decode(
       data.digitalocean_kubernetes_cluster.harambe.kube_config[0].client_certificate
